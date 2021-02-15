5ba4b77852584b1fa8bd3b5cf924d1f7efc649ee
Joe Foster <jfoster29492@gmail.com>
2021-02-15 15:23:51 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Pet = (props) => {
+  return(
+    <div>
+      <p>My pet is {props.petName}</p>
+    </div>
+  )
+}
+
+const Person = (props) => {
+  return (
+    <div>
+      <p>My name is {props.name} and I am {props.age} year(s) old!</p>
+      <Pet petName = {props.pet} />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1>My React Project</h1>
+    <Person name = "Larry" age = {25} pet = "Poppy"/>
+    <Person name = "Curly" age = {26} pet = "Prince"/>
+    <Person name = "Mo" age = {25} pet = "Bessie"/>
     </div>
   );
 }
 
 export default App;
+
+//the class component below is equivalent to the function component above
+
+// class App extends React.Component {
+//   render(){  //must have render method
+//     return(
+//       <div className = "App">
+//         <h1>This is a class component</h1>
+//       </div>
+//     )
+//   }
+// }
+
+
